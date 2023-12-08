ca7dfe9f59aa58675d3f1a1158473a89ba18113d
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-12-07 22:25:39 -0800

    Created a pointcut declaration and implemented it
    test successful

---
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,23 @@
+package com.luv2code.aopdemo.dao;
+
+import com.luv2code.aopdemo.Account;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO{
+
+
+    @Override
+    public void addAccount(Account theAccount, boolean vipFlag) {
+
+        System.out.println(getClass() + ": Doing by db work: adding an account");
+
+    }
+
+    @Override
+    public boolean doWork() {
+        System.out.println(getClass() + ": doWork()");
+        return false;
+    }
+
+}
