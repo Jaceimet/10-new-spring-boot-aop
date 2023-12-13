6770d9bf957321f75b693d37c8f70ef35da6b078
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-12-12 22:52:54 -0800

    testing @AfterReturning
    need to find bug
    
    test fail

--- 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AopdemoApplication {
 
@@ -14,31 +16,47 @@ public class AopdemoApplication {
 		SpringApplication.run(AopdemoApplication.class, args);}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO theAccountDao, MembershipDAO theMembershipDAO){
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO){
 
 		return runner->{
 
-			demoTheBeforeAdvice(theAccountDao, theMembershipDAO);
+			//demoTheBeforeAdvice(theAccountDao, theMembershipDAO);
+
+			demoTheAfterReturningAdvice(theAccountDAO);
 
 		};
 	}
 
-	private void demoTheBeforeAdvice(AccountDAO theAccountDao, MembershipDAO theMembershipDAO) {
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+
+		// display the accounts
+		System.out.println("\n\nMain Program: demoTheAfterReturningAdvice");
+		System.out.println("----");
+
+		System.out.println(theAccounts);
+
+		System.out.println("\n");
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		// call the business method
 		Account myAccount = new Account();
 		myAccount.setName("Madhu");
 		myAccount.setLevel("Platinum");
 
-		theAccountDao.addAccount(myAccount, true);
-		theAccountDao.doWork();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
 
 		// call the accountdao getter/setter methods
-		theAccountDao.setName("foobar");
-		theAccountDao.setServiceCode("silver");
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
 
-		String name = theAccountDao.getName();
-		String code = theAccountDao.getServiceCode();
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
 
 
 		// call the membership business method
